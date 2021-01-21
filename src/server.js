bcd30bb0103726861e4c4c94c8f760d6c317f6d1
XolaniMgube <xnmgube@gmail.com>
2021-01-21 18:21:17 +0200

    mid lesson 4

---
+++ src/server.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+
+    // Set header content type..............................
+    // For writing plain text...............................
+    // res.setHeader('Content-Type', 'text/plain')
+    // res.write('Hello dawg')
+    // res.end()
+
+    // for writing html text...........................
+    // res.setHeader('Content-Type', 'text/html')
+    // res.write('Fede Fede')
+    // res.write('<p>Xolani</p>')
+    // res.end()
+
+    //For sending an html file 
+    res.setHeader('Content-Type', 'text/html')
+
+    let path = '../views/'
+
+    if (req.url == '/') {
+        path += 'index.html'
+    } else if (req.url == '/about') {
+        path += 'about.html'
+    } else {
+        path += '404.html'
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end()
+        } else {
+            res.write(data)
+            res.end() // we can also put data inside the end brackes it will still work
+        }
+    })
+})
+
+// Status Codes
+// 100 Range - information responses
+// 200 Range - success codes
+// 300 Range - codes for redirects
+// 400 Range - user or clients error codes
+// 500 Range - server error codes 
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening on port 3000')
+})
